c1233ac63ca4cb9c768b72c80af3c444703c7f33
jwong <jeffrey.wong@mycom-osi.com>
2020-09-18 08:06:10 +0100

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import React, { Component } from 'react';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
-
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
     function RenderDish({dish}){
@@ -20,13 +21,13 @@ import {Link} from 'react-router-dom';
 
 
 
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}){
         console.log('comments: ', comments)
         if(comments != null){
             return(
                 <div className='col-12 col-md-5 m-1'>
                     <h4>Comments</h4>
-                    <ul className='list-group list-unstyled'>
+                    <ul className='list-group list-unstyled mb-3'>
                         {comments.map((comments) =>{
                             return(
                                 <div>
@@ -40,6 +41,7 @@ import {Link} from 'react-router-dom';
                             )
                         })}
                     </ul>
+                    <CommentForm addComment={addComment} dishId={dishId}/>
                 </div>
             );
         } else{
@@ -55,9 +57,103 @@ import {Link} from 'react-router-dom';
 
 
 
+
+const commentMinLength = (len) => (values) => (values) && (values.length >= len);
+const commentMaxLength = (len) => (val) => !(val) || (val.length <= len);
+
+
+class CommentForm extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            commentModalToggle: false
+        }
+        this.commentModal = this.commentModal.bind(this);
+        this.commentSubmit = this.commentSubmit.bind(this);
+        
+    }
+
+
+    commentModal(commentModalToggle) {
+        this.setState({
+            commentModalToggle: !commentModalToggle
+        })
+    }
+
+    commentSubmit(values){
+        this.commentModal(this.state.commentModalToggle);
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
+    }
+
+
+    render(){
+        return(
+            <div>
+                <Button outline color='secondary' onClick={()=>this.commentModal(this.state.commentModalToggle)} ><span className='fa fa-pencil'></span> Button</Button>
+
+                <Modal isOpen={this.state.commentModalToggle} toggle={this.commentModal}>
+                    <ModalHeader  toggle={this.commentModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <div className='container'>
+                            <LocalForm onSubmit={(values)=>this.commentSubmit(values)}>
+                                <Row className='form-group'>
+                                    <Label htmlFor='rating'>Rating</Label>
+                                    <Control.select model='.rating' className='form-control' id='rating' name='rating'>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Row>
+                                <Row className='form-group'>
+                                    <Label htmlFor='author'>Your Name</Label>
+                                    <Control.text model='.author' className='form-control' id='author' name='author' validators={{commentMinLength:commentMinLength(3), commentMaxLength: commentMaxLength(15)}}/>
+                                    <Errors className='text-danger' model='.author' show='touched' messages={{commentMinLength:'Must be greater than 3 characters.', commentMaxLength:'Must be less than or equal to 15 characters'}} />
+                                </Row>
+                                <Row className='form-group'>
+                                    <Label htmlFor='comment'>Comment</Label>
+                                    <Control.textarea model='.comment' id='comment' name='comment' rows='12' className='form-control'/>
+                                </Row>
+                                <Row>
+                                    <Button type='submit' color='primary'>Submit</Button>
+                                </Row>
+                            </LocalForm>
+                        </div>
+                    </ModalBody>
+                </Modal>
+            </div>
+            );
+    }
+
+}
+
+
+
+
+
+
+
 const DishDetail=(props)=> {
     const dish = props.dish;
     console.log('component render did invoked')
+    if (props.isLoading){
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <Loading />
+                </div>
+            </div>
+        );
+    } else if (props.errMess){
+        return(
+            <div className='container'>
+                <div className='row'>
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
     if(dish != null){
         return(
             <div className='container'>
@@ -70,7 +166,7 @@ const DishDetail=(props)=> {
                 <div className='row'>
                     {console.log('dish: ',dish)}
                     <RenderDish dish={dish} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} addComment = {props.addComment} dishId={props.dish.id}/>
                 </div>
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Contact from './ContactComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import About from './AboutComponent';
 import { connect } from 'react-redux';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -21,17 +22,31 @@ const mapStateToProps = state => {
     }
 };
 
+
+const mapDispatchToProps = (dispatch) => ({
+  addComment:(dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () =>{dispatch(fetchDishes())}
+});
+
+
+
 class Main extends Component {
   constructor(props){
     super(props);
   };
 
 
+  componentDidMount(){
+    this.props.fetchDishes();
+  }
+
 
   render(){
     const HomePage = ()=> {
       return(
-        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+        <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading = {this.props.dishes.isLoading}
+              dishesErrMess = {this.props.dishes.errMess}
               promotions={this.props.promotions.filter((promo) => promo.featured)[0]}
               leaders={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
@@ -41,8 +56,11 @@ class Main extends Component {
 
     const DishwithId = ({match}) =>{
       return(
-        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                    isLoading = {this.props.dishes.isLoading}
+                    errMess = {this.props.dishes.errMess}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                    addComment= {this.props.addComment}
         />
       );    
     };
@@ -70,4 +88,4 @@ class Main extends Component {
 }
 
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
